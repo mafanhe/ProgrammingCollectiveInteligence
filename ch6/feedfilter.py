0c752fa86ed48d3c2b37fba203ec3bd7ca275137
mafanhe <827149941@qq.com>
2016-03-29 23:58:56 +0800

    modify

--- ch6/feedfilter.py
+++ ch6/feedfilter.py
@@ -33,3 +33,30 @@ def entryfeatures(entry):
 
     # 提取标题中的单词并进行标示
     titlewords=[s.lower() for s in splitter.split(entry['title']) if 2 < len(s) > 20]
+    for w in titlewords:
+        f['Title:'+w] = 1
+
+    # 提取摘要中的单词
+    summarywords = [s.lower() for s in splitter.split(entry['summary']) if 2 < len(s) < 20]
+
+    # 统计大写单词
+    uc = 0
+    for i in range(len(summarywords)):
+        w = summarywords[i]
+        f[w] = 1
+        if w.isupper():
+            uc += 1
+
+        # 将从摘要中获得的词组作为特征
+        if i < len(summarywords) -1:
+            twowords = ' '.join(summarywords[i:i+1])
+            f[twowords] = 1
+
+    # 保持文章创建者和发布者名字的完整性
+    f['Publisher:' + entry['publisher']] = 1
+
+    # UPPERCASE是一个“虚拟”单词，用以指示存在过多的大写内容
+    if float(uc)/len(summarywords) > 0.3:
+        f['IPPERCASE'] = 1
+
+    return f
